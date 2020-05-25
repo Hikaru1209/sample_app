cd14ea09585ade139eab25ee7d2ccd7a1cf2e1d5
Hikaru1209 <h.nito2310@gmail.com>
2020-05-25 18:03:21 +0900

    [finish]3-10

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '8a647806873aa3ff3c698fec7220ae98ec3943434d0feb5d337132f90f3e3438da208ee6ef10fd698eb96c18c5b27e22150a7f6456ca18d31e9270667a1ccc97'
---
+++ db/migrate/20200524104222_add_title_to_lists.rb
@@ -0,0 +1,4 @@
+class AddTitleToLists < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
